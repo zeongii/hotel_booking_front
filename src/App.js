7b4901f623a05fbd5f31c92fa9b8684fc29ec860
sbn04043 <sbn04043@gmail.com>
2024-08-12 18:50:22 +0900

    hotel search page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import Auth from "./user/Auth";
 import ShowList from "./hotel/ShowList";
 import ForgotEmail from "./user/ForgotEmail";
 import ForgotPassword from "./user/ForgotPassword";
-import Auth1 from "./user/Auth1";
 import Register from "./user/Register";
+import SearchHotel from "./search/hotel";
 
 function App() {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<Auth1/>}/>
+                <Route path="/" element={<Auth/>}/>
                 <Route path="/hotel/showList" element={<ShowList/>}/>
                 <Route path="/user/register" element={<Register/>}/>
                 <Route path="/user/forgotEmail" element={<ForgotEmail/>}/>
                 <Route path="/user/forgotPassword" element={<ForgotPassword/>}/>
+                <Route path="/search/hotel" element={<SearchHotel/>}/>
             </Routes>
         </div>
     );
